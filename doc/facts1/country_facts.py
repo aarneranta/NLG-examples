8454045cc4fc938523d9f9cd04de160fff8def2a
aarneranta <aarne@chalmers.se>
2022-05-18 07:52:39 +0200

    fixed facts1/country_facts.py

--- doc/facts1/country_facts.py
+++ doc/facts1/country_facts.py
@@ -2,34 +2,37 @@ import pgf
 import json
 
 pgf_file = 'Countries.pgf'
-country_file = '../data/idcountries.tsv'
+country_file = '../data/idcountries.json'
 
 
 def name_expr(name):
-    return pgf.Expr('name' + 'Name', [])
+    return pgf.Expr(name + '_Name', [])
 
 
 def string_expr(s):
     return pgf.readExpr(str('"' + s + '"'))
 
+def name_value(s):
+    return pgf.Expr('NameValue', [name_expr(s)])
+  
   
 def country_facts(c):
-  object = pgf.Expr('NameObject', [name_expr(c.country)])
+  object = pgf.Expr('NameObject', [name_expr(c['country'])])
   return [
     pgf.Expr('AttributeFact', [pgf.Expr(attr,[]), object, val])
       for (attr, val) in [
-        ('capital_Attribute', name_expr(c.capital)),   
-        ('area_Attribute', string_expr(c.area)),
-        ('population_Attribute', string_expr(c.population)),
-        ('continent_Attribute', name_expr(c.continent)),
-        ('currency_Attribute', name_expr(c.currency))
+        ('capital_Attribute', name_value(c['capital'])),   
+        ('area_Attribute', string_expr(c['area'])),
+        ('population_Attribute', string_expr(c['population'])),
+        ('continent_Attribute', name_value(c['continent'])),
+        ('currency_Attribute', name_value(c['currency']))
         ]
     ]
 
 
 def get_countries(filename):
     with open(filename) as file:
-        countries = json.loads(file)
+        countries = json.load(file)
     return countries
 
 
